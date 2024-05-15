d3d3839504e221b4a47a9282dd2640bd63767d09
Bruno Siqueira Pedro Melo <86988869+brd3v@users.noreply.github.com>
2024-05-15 16:15:13 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="App">
+   <div className='header'><h2> AUMENTO GARANTIDO</h2> </div>
+    <h3>Confirme a sua idade para continuar</h3>
+
+    <img src='https://www.alucare.fr/wp-content/uploads/2022/09/18.jpg'></img>
+    <div className='button'>
+      <a href='https://app.monetizze.com.br/r/BCE1806690'><button>  Tenho 18 anos ou mais </button></a>
+    <a href='https://app.monetizze.com.br/r/BCE1806690'>  <button> Tenho Menos de 18 anos </button></a>
+
+      </div>
+
+      <footer>
+        <img src='https://algodaocru.com.br/wp-content/uploads/2022/03/COMPRA-100-SEGURA-1800-px-%C3%97-140-px.png'></img>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
